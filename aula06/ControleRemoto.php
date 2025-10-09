4fff935944581c2a06b9252d2583ba423f434459
Emerson Santos <ebsantoss@gmail.com>
2025-10-09 10:20:40 -0300

    aula6

---
+++ aula06/ControleRemoto.php
@@ -0,0 +1,124 @@
+    <?php 
+    require_once 'Controlador.php';
+    class ControleRemoto implements Controlador {
+        //Atributos
+        public $volume;
+        public $ligado;
+        public $tocando;
+    
+    //Metodos Especiais
+    function __construct() {
+        $this->volume = 50;
+        $this->ligado = false;
+        $this->tocando = false;
+    }
+    function getVolume(){
+    return $this->volume;
+
+    }
+
+    function getLigado(){
+    return $this->ligado;
+
+    }
+
+    function getTocando(){
+    return $this->tocando;
+
+    }
+
+    function setVolume($volume) {
+        $this->volume = $volume;
+    }
+
+    function setLigado($ligado) {
+        $this->ligado = $ligado;
+    }
+
+    function setTocando($tocando) {
+        $this->tocando = $tocando;
+    }
+
+    //Metodos do Contolador
+    public function ligar() {
+        $this->setLigado(true);
+
+    } 
+
+    public function desligar() {
+        $this->setLigado(false);
+        
+    }
+
+    public function abrirMenu() {
+        echo "<br>Está ligado?: " . ($this->getLigado()?"SIM":"NÃO");
+        echo "<br>Está tocando?: " . ($this->getTocando()?"SIM":"NÃO");
+        echo "<br>Volume: " . $this->getVolume(); 
+          for($i=0; $i <= $this->getVolume(); $i +=10 ) {
+            echo " | ";
+          }
+        echo "<br>";
+    }
+
+    public function fecharMenu() {
+        echo "<br>Fechando Menu...";
+        
+    }
+
+    public function maisVolume() {
+        if ($this->getLigado()) {
+            $this->setVolume($this->getVolume() + 5);
+        } else {
+            echo "<p>ERRO! Não posso aumentar o volume</p>";
+        }
+
+        
+    }
+
+    public function menosVolume() {
+        if ($this->getLigado()) {
+            $this->setVolume($this->getVolume() - 5);
+        } else {
+            echo "<p>ERRO! Não posso diminuir o volume</p>";
+        }
+        
+    }
+
+    public function ligarMudo() {
+        if ($this->getLigado() && $this->getVolume() > 0) {
+            $this->setVolume(0);
+        }
+        
+    }
+
+    public function desligarMudo() {
+        if ($this->getLigado() && $this->getVolume() == 0) {
+            $this->setVolume(50);
+        }
+        
+    }
+
+    public function play() {
+        if ($this->getLigado() && ! ($this->getTocando())) {
+            $this->setTocando(true);
+        }
+        
+    }
+
+    public function pause() {
+        if ($this->getLigado() && ($this->getTocando())) {
+            $this->setTocando(false);
+        }
+     
+    } 
+
+    }
+
+    
+
+
+
+
+    
+    
+    ?>
--- aula06/index.php
+++ aula06/index.php
@@ -6,41 +6,15 @@
     <title>Document</title>
 </head>
 <body>
+    <h1>Projeto Remoto</h1>
+
     <pre>
         <?php
-        require_once 'ContaBanco.php';
-        $p1 = new ContaBanco(); //Jubileu
-        $p2 = new ContaBanco(); // Creuza
-        $p1->abrirConta("CC");
-        $p1->setNumConta(1111);
-        $p1->setDono("Jubileu");
-                $p2->abrirConta("CP");
-        $p2->setNumConta(2222);
-        $p2->setDono("Creuza");
-
-        $p1->depositar(300);
-        $p2->depositar(500);
-
-        $p2->sacar(1000);
-
-        $p1->pagarMensal();
-        $p2->pagarMensal();
-
-        $p1->sacar(338);
-        $p2->sacar(630);
-
-        $p1->fecharConta();
-        $p2->fecharConta();
-
-
-        echo "</br>";
-        echo "</br>";
-        print_r($p1);
-        print_r($p2);
-        
-        
-        
-        
+        require_once 'ControleRemoto.php';
+        $c = new ControleRemoto();
+        $c->ligar();
+        $c->maisVolume();
+        $c->abrirMenu();
         ?>
     </pre>
 </body>
