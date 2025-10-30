2250f2633f4f2d6c3fdea6b77dbca69b5de1f6c6
Emerson Santos <ebsantoss@gmail.com>
2025-10-30 15:49:24 -0300

    estudos

---
+++ Treinamentos/tr7/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="salario.php" method="get">
+    <label for="salario">Informe seu salario</label>
+    <input type="number" name="salario" id="salario">
+
+    <input type="submit" value="Salario">
+
+
+
+    </form>
+    
+</body>
+</html>
